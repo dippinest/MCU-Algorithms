ca0510849ae142e6b0075e16fff7dd5653e6628d
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-01 20:01:44 +0300

    Update filters.h

--- Libraries/filters/filters.h
+++ Libraries/filters/filters.h
@@ -1,4 +1,16 @@
 
+
+// ===============================================================================
+//
+// Библиотека с реализацией простейших цифровых фильтров
+//
+// -------------------------------------------------------------------------------
+//
+// Library with implementation of the simplest digital filters
+//
+// ===============================================================================
+
+
 #ifndef FILTERS_H_
 #define FILTERS_H_
 
@@ -6,10 +18,12 @@
 #include <stdlib.h>
 #include <math.h>
 
+
 // ===============================================================================
 
-// functions for artificial signal noise (can be used in simulation)
 
+
+// functions for artificial signal noise (can be used in simulation)
 
 int16_t Filters_Get_Rand_Noise_Int16(int16_t amp);
 
@@ -21,10 +35,11 @@ float Filters_Get_Splash_Float(float amp, uint16_t frequency);
 
 float Filters_Get_Gaussian_Noise(float amp);
 
+
 // ===============================================================================
 
-// simple moving average (FIR filter)
 
+// simple moving average (FIR filter)
 
 int8_t Filters_SMA_Int8(int8_t *buffer, uint16_t buffer_size, uint16_t *index, int8_t value);
 
@@ -34,10 +49,11 @@ int32_t Filters_SMA_Int32(int32_t *buffer, uint16_t buffer_size, uint16_t *index
 
 float Filters_SMA_Float(float *buffer, uint16_t buffer_size, uint16_t *index, float value);
 
+
 // ===============================================================================
 
-// simple moving median (FIR filter)
 
+// simple moving median (FIR filter)
 
 int8_t Filters_SMM_Int8(int8_t *buffer, uint16_t buffer_size, uint16_t *index, int8_t value);
 
@@ -47,10 +63,11 @@ int32_t Filters_SMM_Int32(int32_t *buffer, uint16_t buffer_size, uint16_t *index
 
 float Filters_SMM_Float(float *buffer, uint16_t buffer_size, uint16_t *index, float value);
 
+
 // ===============================================================================
 
-// linearly weighted moving average (IIR filter)
 
+// linearly weighted moving average (IIR filter)
 
 int8_t Filters_LWMA_Int8(int8_t *buffer, uint16_t buffer_size, uint16_t *index, int16_t value);
 
@@ -60,10 +77,11 @@ int32_t Filters_LWMA_Int32(int32_t *buffer, uint16_t buffer_size, uint16_t *inde
 
 float Filters_LWMA_Float(float *buffer, uint16_t buffer_size, uint16_t *index, float value);
 
+
 // ===============================================================================
 
-// exponentially weighted moving average (IIR filter)
 
+// exponentially weighted moving average (IIR filter)
 
 int8_t Filters_EMA_Int8(int8_t *accumulator, float coef, int8_t value);
 
@@ -73,10 +91,11 @@ int32_t Filters_EMA_Int32(int32_t *accumulator, float coef, int32_t value);
 
 float Filters_EMA_Float(float *accumulator, float coef, float value);
 
+
 // ===============================================================================
 
-// cumulative moving average - simplest IIR filter (digital analog RC filter)
 
+// cumulative moving average - simplest IIR filter (digital analog RC filter)
 
 int8_t Filters_CMA_Int8(int16_t *accumulator, int8_t *old_value, uint16_t filter_order, int8_t value);
 
@@ -86,10 +105,11 @@ int32_t Filters_CMA_Int32(int64_t *accumulator, int32_t *old_value, uint16_t fil
 
 float Filters_CMA_Float(float *accumulator, float *old_value, uint16_t filter_order, float value);
 
+
 // ===============================================================================
 
-// running moving average (IIR filter)
 
+// running moving average (IIR filter)
 
 int8_t Filters_RMA_Int8(int8_t *accumulator, const uint16_t filter_order, int8_t value);
 
@@ -99,9 +119,12 @@ int32_t Filters_RMA_Int32(int32_t *accumulator, const uint16_t filter_order, int
 
 float Filters_RMA_Float(float *accumulator, const uint16_t filter_order, float value);
 
+
 // ===============================================================================
 
-// adaptive exponentially weighted moving average (IIR filter) - here the coefficient adapts to the difference between the current and the previous value
+
+// adaptive exponentially weighted moving average (IIR filter) - here the
+// coefficient adapts to the difference between the current and the previous value
 
 int8_t Filters_Adaptive_EMA_Int8(int8_t *accumulator, int8_t min_value, int8_t max_value, uint8_t noise_max_amp, float coef, int8_t value);
 
@@ -112,3 +135,5 @@ int32_t Filters_Adaptive_EMA_Int32(int32_t *accumulator, int32_t min_value, int3
 float Filters_Adaptive_EMA_Float(float *accumulator, float min_value, float max_value, float noise_max_amp, float coef, float value);
 
 #endif
+
+
