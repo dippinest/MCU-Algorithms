c302f56acba8918d7587f0d7c3ee26f232124724
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-01 20:27:43 +0300

    Update itohexa.h

--- Libraries/numconvert/itohexa/itohexa.h
+++ Libraries/numconvert/itohexa/itohexa.h
@@ -1,4 +1,18 @@
 
+
+// ===============================================================================
+//
+// Библиотека для преобразования различных числовых типов данных в строки
+// C-типа (в шестнадцетиричном виде)
+//
+// -------------------------------------------------------------------------------
+//
+// Library for converting various numeric data types into C-type strings
+// (in hexadecimal form)
+//
+// ===============================================================================
+
+
 #ifndef ITOHEXA_H_
 #define ITOHEXA_H_
 
@@ -7,7 +21,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+
 // ===============================================================================
+
 
 void ITOHEXA_Set_String_Buffer(char *string_buffer_ptr);
 
@@ -15,7 +31,9 @@ char *ITOHEXA_Set_String_Buffer_To_Dynamic_Memory(const uint8_t string_buffer_si
 
 void ITOHEXA_Delete_String_Buffer_To_Dynamic_Memory();
 
+
 // ===============================================================================
+
 
 char *ITOHEXA_8bitNum_To_HexString(uint8_t val, bool is_lower_case);
 
@@ -23,6 +41,14 @@ char *ITOHEXA_16bitNum_To_HexString(uint16_t val, bool is_lower_case);
 
 char *ITOHEXA_32bitNum_To_HexString(uint32_t val, bool is_lower_case);
 
+
+#ifdef NUM_64BIT_SUPPORT
+
 char *ITOHEXA_64bitNum_To_HexString(uint64_t val, bool is_lower_case);
 
 #endif
+
+
+#endif
+
+
