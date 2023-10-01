c16615f5336e50d5a43be0d609bc986d6967e35e
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-01 20:21:24 +0300

    Update itobina.h

--- Libraries/numconvert/itobina/itobina.h
+++ Libraries/numconvert/itobina/itobina.h
@@ -1,4 +1,18 @@
 
+
+// ===============================================================================
+//
+// Библиотека для преобразования различных числовых типов данных в строки
+// C-типа (в двоичном виде)
+//
+// -------------------------------------------------------------------------------
+//
+// Library for converting various numeric data types into C-type strings
+// (in binary form)
+//
+// ===============================================================================
+
+
 #ifndef ITOBINA_H_
 #define ITOBINA_H_
 
@@ -7,7 +21,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+
 // ===============================================================================
+
 
 void ITOBINA_Set_String_Buffer(char *string_buffer_ptr);
 
@@ -15,7 +31,9 @@ char *ITOBINA_Set_String_Buffer_To_Dynamic_Memory(const uint8_t string_buffer_si
 
 void ITOBINA_Delete_String_Buffer_To_Dynamic_Memory();
 
+
 // ===============================================================================
+
 
 char *ITOBINA_8bitNum_To_BinString(uint8_t val);
 
@@ -23,6 +41,14 @@ char *ITOBINA_16bitNum_To_BinString(uint16_t val);
 
 char *ITOBINA_32bitNum_To_BinString(uint32_t val);
 
+
+#ifdef NUM_64BIT_SUPPORT
+
 char *ITOBINA_64bitNum_To_BinString(uint64_t val);
 
 #endif
+
+
+#endif
+
+
