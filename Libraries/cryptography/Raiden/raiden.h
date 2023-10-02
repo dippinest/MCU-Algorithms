39ec75168df58fddf180b7616b8614deda1b0c35
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-02 10:31:59 +0300

    Update raiden.h

--- Libraries/cryptography/Raiden/raiden.h
+++ Libraries/cryptography/Raiden/raiden.h
@@ -1,3 +1,20 @@
+
+
+// ===============================================================================
+//
+// Эта библиотека, реализует криптографический алгоритм алгоритм Raiden.
+// Функции библиотеки позволяют использовать различные режимы
+// шифрования данных: ECB, CBC, PCBC, CFB и OFB
+//
+// -------------------------------------------------------------------------------
+//
+// This library implements the Raiden cryptographic algorithm.
+// The library functions allow you to use various data
+// encryption modes: ECB, CBC, PCBC, CFB and OFB
+//
+// ===============================================================================
+
+
 #ifndef RAIDEN_H_
 #define RAIDEN_H_
 
@@ -23,4 +40,6 @@ uint32_t RAIDEN_Encrypt_OFB(const void *init_vector_64bit, void *data, const uin
 
 uint32_t RAIDEN_Decrypt_OFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
 
-#endif /* RAIDEN_H_ */
+#endif
+
+
