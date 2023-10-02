50f2a61ede02372922c67e5207e64f78c26516fa
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-02 10:03:11 +0300

    Update grayscode.h

--- Libraries/numconvert/grayscode/grayscode.h
+++ Libraries/numconvert/grayscode/grayscode.h
@@ -1,8 +1,24 @@
 
+
+// ===============================================================================
+//
+// Библиотека для преобразования различных целых числовых типов данных
+// в в код Грея и обратно
+//
+// -------------------------------------------------------------------------------
+//
+// Library for converting various integer data types
+// to Gray code and back
+//
+// ===============================================================================
+
+
 #ifndef GRAYSCODE_H_
 #define GRAYSCODE_H_
 
+
 #include <stdint.h>
+
 
 uint8_t GRAYSCODE_UInt8_To_UInt8Grayscode(uint8_t num);
 
@@ -16,8 +32,16 @@ uint32_t GRAYSCODE_UInt32_To_UInt32Grayscode(uint32_t num);
 
 uint32_t GRAYSCODE_UInt32Grayscode_To_UInt32(uint32_t gray_code);
 
+
+#ifdef NUM_64BIT_SUPPORT
+
 uint64_t GRAYSCODE_UInt64_To_UInt64Grayscode(uint64_t num);
 
 uint64_t GRAYSCODE_UInt64Grayscode_To_UInt64(uint64_t gray_code);
 
 #endif
+
+
+#endif
+
+
