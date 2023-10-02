9b6fab50ffeb54a92c1ec967ec5a97383dea5226
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-02 10:21:33 +0300

    Update xtea.h

--- Libraries/cryptography/XTEA/xtea.h
+++ Libraries/cryptography/XTEA/xtea.h
@@ -1,7 +1,26 @@
+
+
+// ===============================================================================
+//
+// Эта библиотека, реализует криптографический алгоритм алгоритм XTEA.
+// Функции библиотеки позволяют использовать различные режимы
+// шифрования данных: ECB, CBC, PCBC, CFB и OFB
+//
+// -------------------------------------------------------------------------------
+//
+// This library implements the XTEA cryptographic algorithm.
+// The library functions allow you to use various data
+// encryption modes: ECB, CBC, PCBC, CFB and OFB
+//
+// ===============================================================================
+
+
 #ifndef XTEA_H_
 #define XTEA_H_
 
+
 #include <stdint.h>
+
 
 uint32_t XTEA_Encrypt_ECB(void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
 
@@ -24,3 +43,5 @@ uint32_t XTEA_Encrypt_OFB(const void *init_vector_64bit, void *data, const uint3
 uint32_t XTEA_Decrypt_OFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit, uint8_t num_of_rounds);
 
 #endif
+
+
