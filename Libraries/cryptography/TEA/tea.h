aa0c1d421f4fb9b538e45ba238dcda7ad86a436f
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-02 10:19:21 +0300

    Update tea.h

--- Libraries/cryptography/TEA/tea.h
+++ Libraries/cryptography/TEA/tea.h
@@ -1,7 +1,26 @@
+
+
+// ===============================================================================
+//
+// Эта библиотека, реализует криптографический алгоритм алгоритм TEA.
+// Функции библиотеки позволяют использовать различные режимы
+// шифрования данных: ECB, CBC, PCBC, CFB и OFB
+//
+// -------------------------------------------------------------------------------
+//
+// This library implements the TEA cryptographic algorithm.
+// The library functions allow you to use various data
+// encryption modes: ECB, CBC, PCBC, CFB and OFB
+//
+// ===============================================================================
+
+
 #ifndef TEA_H_
 #define TEA_H_
 
+
 #include <stdint.h>
+
 
 uint32_t TEA_Encrypt_ECB(void *data, const uint32_t data_size, const void *key_128bit);
 
@@ -24,3 +43,5 @@ uint32_t TEA_Encrypt_OFB(const void *init_vector_64bit, void *data, const uint32
 uint32_t TEA_Decrypt_OFB(const void *init_vector_64bit, void *data, const uint32_t data_size, const void *key_128bit);
 
 #endif
+
+
