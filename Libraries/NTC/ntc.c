9af49dfe90e365fa9abd11428e433155faa8baf1
Denis Chicherov <dippinest.workmail@gmail.com>
2023-11-24 20:06:01 +0300

    Update ntc.c

--- Libraries/NTC/ntc.c
+++ Libraries/NTC/ntc.c
@@ -1,14 +1,25 @@
 
 #include "ntc.h"
 
-float NTC_Get_Temperature_To_Kelvin(uint16_t adc_value)
+
+float NTC_Get_Temperature_To_Kelvin
+(
+	uint16_t adc_value,
+	uint16_t adc_max_val,
+	float    U_ref,
+	float    U_gen,
+	float    R_serial,
+	float    A_coef,
+	float    B_coef,
+	float    C_coef
+)
 {
-	float U  = ((float)adc_value / NTC_ADC_MAX_VAL) * NTC_U_REF;
-	float R  = NTC_SERIAL_R * (U / (NTC_U_GEN - U));
+	float U  = ((float)adc_value / adc_max_val) * U_ref;
+	float R  = R_serial * (U / (U_gen - U));
 	
 	float lnR = log(R);
 	
-	return 1.0 / (NTC_A_COEF + (NTC_B_COEF * lnR) + (NTC_C_COEF * (lnR * lnR * lnR)));
+	return 1.0 / (A_coef + (B_coef * lnR) + (C_coef * (lnR * lnR * lnR)));
 }
 
 
