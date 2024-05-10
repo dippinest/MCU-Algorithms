2970d5297e2027f94b863a6a967a6f722518bc68
Denis Chicherov <dippinest.workmail@gmail.com>
2024-05-10 14:20:58 +0300

    Update mathx.h

--- Libraries/Numconvert/mathx/mathx.h
+++ Libraries/Numconvert/mathx/mathx.h
@@ -2,9 +2,16 @@
 
 // ===============================================================================
 //
-// Библиотека с полезными математическими функциями, которых нет в стандартной библиотеке math.h
+// Библиотека с полезными математическими функциями
 //
 // На данный момент библиотека содержит следующие функции:
+//
+//
+//      MATHX_Get_Min_X : возвращает меньшее из двух чисел
+//
+//      MATHX_Get_Max_X : возвращает большее из двух чисел
+//
+//      MATHX_Get_Abs_X : возвращает модуль числа
 //
 //		MATHX_Constrain_X : возвращает число с учётом области допустимых значений
 //
@@ -13,9 +20,15 @@
 //
 // -------------------------------------------------------------------------------
 //
-// A library with useful mathematical functions that are not available in the standard math.h library
+// Library with useful mathematical functions
 //
 // At the moment, the library contains the following functions:
+//
+//      MATHX_Get_Min_X : returns a smaller of the two numbers
+//
+//      MATHX_Get_Max_X : returns a larger of two numbers
+//
+//      MATHX_Get_Abs_X : returns a absolute value
 //
 //		MATHX_Constrain_X : returns a number taking into account the range of acceptable values
 //
@@ -30,25 +43,111 @@
 
 #include <stdint.h>
 
-int16_t MATHX_Constraint_Int16(int16_t val, int16_t min, int16_t max);
 
-uint16_t MATHX_Constraint_UInt16(uint16_t val, uint16_t min, uint16_t max);
 
-int32_t MATHX_Constraint_Int32(int32_t val, int32_t min, int32_t max);
+int8_t MATHX_Get_Min_Int8(int8_t x1, int8_t x2);
 
-uint32_t MATHX_Constraint_UInt32(uint32_t val, uint32_t min, uint32_t max);
+uint8_t MATHX_Get_Min_UInt8(uint8_t x1, uint8_t x2);
+
+int16_t MATHX_Get_Min_Int16(int16_t x1, int16_t x2);
+
+uint16_t MATHX_Get_Min_UInt16(uint16_t x1, uint16_t x2);
+
+int32_t MATHX_Get_Min_Int32(int32_t x1, int32_t x2);
+
+uint32_t MATHX_Get_Min_UInt32(uint32_t x1, uint32_t x2);
 
 
 #ifdef NUM_64BIT_SUPPORT
 
-int64_t MATHX_Constraint_Int64(int64_t val, int64_t min, int64_t max);
+int64_t MATHX_Get_Min_Int64(int64_t x1, int64_t x2);
 
-uint64_t MATHX_Constraint_UInt64(uint64_t val, uint64_t min, uint64_t max);
+uint64_t MATHX_Get_Min_UInt64(uint64_t x1, uint64_t x2);
 
 #endif
 
 
-float MATHX_Constraint_Float(float val, float min, float max);
+float MATHX_Get_Min_Float(float x1, float x2);
+
+
+
+// ===============================================================================
+
+
+int8_t MATHX_Get_Max_Int8(int8_t x1, int8_t x2);
+
+uint8_t MATHX_Get_Max_UInt8(uint8_t x1, uint8_t x2);
+
+int16_t MATHX_Get_Max_Int16(int16_t x1, int16_t x2);
+
+uint16_t MATHX_Get_Max_UInt16(uint16_t x1, uint16_t x2);
+
+int32_t MATHX_Get_Max_Int32(int32_t x1, int32_t x2);
+
+uint32_t MATHX_Get_Max_UInt32(uint32_t x1, uint32_t x2);
+
+
+#ifdef NUM_64BIT_SUPPORT
+
+int64_t MATHX_Get_Max_Int64(int64_t x1, int64_t x2);
+
+uint64_t MATHX_Get_Max_UInt64(uint64_t x1, uint64_t x2);
+
+#endif
+
+
+float MATHX_Get_Max_Float(float x1, float x2);
+
+
+
+// ===============================================================================
+
+
+int8_t MATHX_Get_Abs_Int8(int8_t num);
+
+int16_t MATHX_Get_Abs_Int16(int16_t num);
+
+int32_t MATHX_Get_Abs_Int32(int32_t num);
+
+
+#ifdef NUM_64BIT_SUPPORT
+
+int64_t MATHX_Get_Abs_Int64(int64_t num);
+
+#endif
+
+
+float MATHX_Get_Abs_Float(float num);
+
+
+
+// ===============================================================================
+
+
+int8_t MATHX_Constrain_Int8(int8_t val, int8_t min, int8_t max);
+
+uint8_t MATHX_Constrain_UInt8(uint8_t val, uint8_t min, uint8_t max);
+
+int16_t MATHX_Constrain_Int16(int16_t val, int16_t min, int16_t max);
+
+uint16_t MATHX_Constrain_UInt16(uint16_t val, uint16_t min, uint16_t max);
+
+int32_t MATHX_Constrain_Int32(int32_t val, int32_t min, int32_t max);
+
+uint32_t MATHX_Constrain_UInt32(uint32_t val, uint32_t min, uint32_t max);
+
+
+#ifdef NUM_64BIT_SUPPORT
+
+int64_t MATHX_Constrain_Int64(int64_t val, int64_t min, int64_t max);
+
+uint64_t MATHX_Constrain_UInt64(uint64_t val, uint64_t min, uint64_t max);
+
+#endif
+
+
+float MATHX_Constrain_Float(float val, float min, float max);
+
 
 
 // ===============================================================================
