daf1ec8bb9f869df032e5647b050e2e927e8c1d9
Denis Chicherov <dippinest.workmail@gmail.com>
2025-01-16 18:28:54 +0300

    Update main.c

--- Libraries/Numconvert/itoa/Examples/For AVR/Convert 64-bits numbers/main.c
+++ Libraries/Numconvert/itoa/Examples/For AVR/Convert 64-bits numbers/main.c
@@ -1,6 +1,4 @@
 
-#include <stdbool.h>
-
 #include "uart.h"
 #include "itoa.h"
 #include "deftypes.h"
@@ -13,18 +11,19 @@ int main(void)
 	
 	UART_String_Transmit("Converting 64-bits numbers to C-type string\r\n\r\n");
 	
-	UART_String_Transmit("Macro \"MIN_QWORD_VAL\"     = "); UART_StringLn_Transmit(ITOA_UInt64_To_String(MIN_QWORD_VAL,     21));
-	UART_String_Transmit("Macro \"MAX_QWORD_VAL\"     = "); UART_StringLn_Transmit(ITOA_UInt64_To_String(MAX_QWORD_VAL,     21));
-	UART_String_Transmit("Macro \"MIN_SQWORD_VAL\"    = "); UART_StringLn_Transmit(ITOA_Int64_To_String(MIN_SQWORD_VAL,     21));
-	UART_String_Transmit("Macro \"MAX_SQWORD_VAL\"    = "); UART_StringLn_Transmit(ITOA_Int64_To_String(MAX_SQWORD_VAL,     21));
+	
 	UART_String_Transmit("Macro \"MIN_INT64_VAL\"     = "); UART_StringLn_Transmit(ITOA_Int64_To_String(MIN_INT64_VAL,      21));
 	UART_String_Transmit("Macro \"MAX_INT64_VAL\"     = "); UART_StringLn_Transmit(ITOA_Int64_To_String(MAX_INT64_VAL,      21));
+	
 	UART_String_Transmit("Macro \"MIN_UINT64_VAL\"    = "); UART_StringLn_Transmit(ITOA_UInt64_To_String(MIN_UINT64_VAL,    21));
 	UART_String_Transmit("Macro \"MAX_UINT64_VAL\"    = "); UART_StringLn_Transmit(ITOA_UInt64_To_String(MAX_UINT64_VAL,    21));
-	UART_String_Transmit("Macro \"MIN_S64_VAL\"       = "); UART_StringLn_Transmit(ITOA_Int64_To_String(MIN_S64_VAL,        21));
-	UART_String_Transmit("Macro \"MAX_S64_VAL\"       = "); UART_StringLn_Transmit(ITOA_Int64_To_String(MAX_S64_VAL,        21));
+	
+	UART_String_Transmit("Macro \"MIN_I64_VAL\"       = "); UART_StringLn_Transmit(ITOA_Int64_To_String(MIN_I64_VAL,        21));
+	UART_String_Transmit("Macro \"MAX_I64_VAL\"       = "); UART_StringLn_Transmit(ITOA_Int64_To_String(MAX_I64_VAL,        21));
+	
 	UART_String_Transmit("Macro \"MIN_U64_VAL\"       = "); UART_StringLn_Transmit(ITOA_UInt64_To_String(MIN_U64_VAL,       21));
 	UART_String_Transmit("Macro \"MAX_U64_VAL\"       = "); UART_StringLn_Transmit(ITOA_UInt64_To_String(MAX_U64_VAL,       21));
+	
 	UART_String_Transmit("Macro \"MIN_NATURAL64_VAL\" = "); UART_StringLn_Transmit(ITOA_UInt64_To_String(MIN_NATURAL64_VAL, 21));
 	UART_String_Transmit("Macro \"MAX_NATURAL64_VAL\" = "); UART_StringLn_Transmit(ITOA_UInt64_To_String(MAX_NATURAL64_VAL, 21));
 	
@@ -35,3 +34,6 @@ int main(void)
 	{
 	}
 }
+
+
+
