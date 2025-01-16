088142a2116d999264340ef45d4ffb174ca55084
Denis Chicherov <dippinest.workmail@gmail.com>
2025-01-16 04:56:39 +0300

    Update main.c

--- Libraries/CRC/Examples/For AVR/main.c
+++ Libraries/CRC/Examples/For AVR/main.c
@@ -1,7 +1,4 @@
 
-#define F_CPU 16000000UL
-#include <util/delay.h>
-
 #include "uart.h"
 #include "itohexa.h"
 #include "crc.h"
@@ -17,18 +14,24 @@ typedef struct
 
 static char str_buf[16];
 
+
 int main(void)
 {
 	UART_Initialize(9600, true, false);
+	
 	ITOHEXA_Set_String_Buffer(str_buf);
+	
 	
 	Data d = { 'a', 232, 7633, 684567387573458769 };
 	
+	
 	UART_StringLn_Transmit(
 		ITOHEXA_32bitNum_To_HexString(
-			CRC32_Get(&d, sizeof(Data), 0xA0435757, 0xFF457436, true, false, 0x00446774), false));
+			CRC32_Get_Value(&d, sizeof(Data), 0xA0435757, 0xFF457436, true, false, 0x00446774), false));
 	
 	while (1)
 	{
 	}
 }
+
+
