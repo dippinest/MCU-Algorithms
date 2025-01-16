daa647f79943f6c0d7aaba359752902e9e5fa522
Denis Chicherov <dippinest.workmail@gmail.com>
2025-01-16 04:51:49 +0300

    Update crc.h

--- Libraries/CRC/crc.h
+++ Libraries/CRC/crc.h
@@ -31,16 +31,16 @@
 // "refout"     determines whether the order of the register bits is inverted when entering the XOR element
 // "xor_out"    the value with which the resulting result is added modulo 2 (XOR op)
 
-uint8_t CRC8_Get(void *data, uint16_t data_size, uint8_t polynomial, uint8_t initial, bool refin, bool refout, uint8_t xor_out);
+uint8_t CRC8_Get_Value(void *data, uint16_t data_size, uint8_t polynomial, uint8_t initial, bool refin, bool refout, uint8_t xor_out);
 
-uint16_t CRC16_Get(void *data, uint16_t data_size, uint16_t polynomial, uint16_t initial, bool refin, bool refout, uint16_t xor_out);
+uint16_t CRC16_Get_Value(void *data, uint16_t data_size, uint16_t polynomial, uint16_t initial, bool refin, bool refout, uint16_t xor_out);
 
-uint32_t CRC32_Get(void *data, uint16_t data_size, uint32_t polynomial, uint32_t initial, bool refin, bool refout, uint32_t xor_out);
+uint32_t CRC32_Get_Value(void *data, uint16_t data_size, uint32_t polynomial, uint32_t initial, bool refin, bool refout, uint32_t xor_out);
 
 
 #ifdef NUM_64BIT_SUPPORT
 
-uint64_t CRC64_Get(void* data, uint16_t data_size, uint64_t polynomial, uint64_t initial, bool refin, bool refout, uint64_t xor_out);
+uint64_t CRC64_Get_Value(void* data, uint16_t data_size, uint64_t polynomial, uint64_t initial, bool refin, bool refout, uint64_t xor_out);
 
 #endif
 
