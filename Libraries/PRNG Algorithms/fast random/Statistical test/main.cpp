24267f4ed0e757e22c80099c7ad5a7954d37dd3e
Denis Chicherov <dippinest.workmail@gmail.com>
2025-08-01 05:55:41 +0300

    Update main.cpp

--- Libraries/PRNG Algorithms/fast random/Statistical test/main.cpp
+++ Libraries/PRNG Algorithms/fast random/Statistical test/main.cpp
@@ -4,19 +4,19 @@
 #include "fastrandom.h"
 
 
-const size_t NUM_OF_VALUES = 10000;
+const size_t NUM_OF_VALUES = 100000;
 
 
 int main()
 {
 	FASTRandom_Set_Seed(0x0000);
 
-	// генерирация 10.000 чисел в диапазоне от 0 до 256
+	// генерирация 10.000 чисел в диапазоне от 0 до 255
 	// -------------------------------------------------------------------------------
-	// generating 10,000 numbers in the range from 0 to 256
+	// generating 10,000 numbers in the range from 0 to 255
 	for (size_t i = 0; i < NUM_OF_VALUES; i++)
 	{
-		std::cout << FASTRandom_Get_UInt16_Value(0, 256) << ", ";
+		std::cout << FASTRandom_Get_UInt16_Value(0, (255 + 1)) << ", ";
 	}
 }
 
